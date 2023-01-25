90f45630645ae18c1ab9caca23e74968e2eef758
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-25 15:43:23 +0200

    Showing Feedback To The User

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -10,6 +10,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -23,6 +24,7 @@ const AuthForm = () => {
 
     console.log(enteredEmail, enteredPassword);
 
+    setIsLoading(true);
     if (isLogin) {
       return;
     } else { 
@@ -39,11 +41,17 @@ const AuthForm = () => {
           }
         }
       ).then(res => {
+        setIsLoading(false);
         if (res.ok) {
 
         } else {
           return res.json().then(data => {
-            console.log(data); // show error;
+            console.log(data.error.message); // show error;
+            let errorMessage = 'Authentication failed!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
           });
         }
       });
@@ -63,7 +71,8 @@ const AuthForm = () => {
           <input type='password' id='password' required ref={passwordInputRef}/>
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+         { !isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+         { isLoading && <p>Sending request...</p>}
           <button
             type='button'
             className={classes.toggle}
@@ -79,43 +88,3 @@ const AuthForm = () => {
 
 export default AuthForm;
 
-
-// import { useState } from 'react';
-
-// import classes from './AuthForm.module.css';
-
-// const AuthForm = () => {
-//   const [isLogin, setIsLogin] = useState(true);
-
-//   const switchAuthModeHandler = () => {
-//     setIsLogin((prevState) => !prevState);
-//   };
-
-//   return (
-//     <section className={classes.auth}>
-//       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-//       <form>
-//         <div className={classes.control}>
-//           <label htmlFor='email'>Your Email</label>
-//           <input type='email' id='email' required />
-//         </div>
-//         <div className={classes.control}>
-//           <label htmlFor='password'>Your Password</label>
-//           <input type='password' id='password' required />
-//         </div>
-//         <div className={classes.actions}>
-//           <button>{isLogin ? 'Login' : 'Create Account'}</button>
-//           <button
-//             type='button'
-//             className={classes.toggle}
-//             onClick={switchAuthModeHandler}
-//           >
-//             {isLogin ? 'Create new account' : 'Login with existing account'}
-//           </button>
-//         </div>
-//       </form>
-//     </section>
-//   );
-// };
-
-// export default AuthForm;
